0db12734a4f97e52acb1a56c63079414b4eaa203
Ryan Zoerb <ryan.zoerb@gmail.com>
2014-01-18 14:55:19 -0600

    Om up and running

--- project.clj
+++ project.clj
@@ -3,17 +3,34 @@
   :url "http://example.com/FIXME"
   :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2127"]
+                 [org.clojure/tools.reader "0.8.2"]
+                 [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [compojure "1.1.6"]]
+                 [ring/ring-core "1.2.1"]
+                 [compojure "1.1.6"]
+                 [cljs-http "0.1.2"]
+                 [cheshire "5.3.1"]
+                 [om "0.1.7"]]
   :plugins [[lein-cljsbuild "1.0.1"]
             [lein-ring "0.8.8"]]
   :cljsbuild {
-    :builds [{:source-paths ["src/cljs"]
+    :builds [{:id "dev"
+              :source-paths ["src/cljs"]
               :compiler {
-                :output-dir "resources/public/js"
                 :output-to "resources/public/js/virt-clj.js"
-                :source-map "resources/public/js/virt-clj.cljs"
-                :optimizations :whitespace
-                :pretty-print true}}]}
-  :ring {:handler virt-clj.core/handler})
+                :output-dir "resources/public/js/out"
+                :optimizations :none
+                :externs ["om/externs/react.js"]}}
+             {:id "release"
+              :source-paths ["src/cljs"]
+              :compiler {
+                :output-to "resources/public/js/virt-vlj.js"
+                :source-map "resources/public/js/virt-vlj.js.map"
+                :optimizations :advanced
+                :pretty-print false
+                :output-wrapper false
+                :preamble ["om/react.min.js"]
+                :externs ["om/externs/react.js"]
+                :closure-warnings
+                {:non-standard-jsdoc :off}}}]}
+  :ring {:handler virt-clj.core/app})
--- src/cljs/virt_clj/core.cljs
+++ src/cljs/virt_clj/core.cljs
@@ -1,3 +1,23 @@
-(ns virt-clj.core)
+(ns virt-clj.core
+    (:require-macros [cljs.core.async.macros :refer [go alt!]])
+    (:require [goog.events :as events]
+              [cljs.core.async :refer [put! <! >! chan timeout]]
+              [om.core :as om :include-macros true]
+              [om.dom :as dom :include-macros true]
+              [cljs-http.client :as http]
+              [virt-clj.utils :refer [guid]]))
 
-(. js/console (log "Hello world!"))
+
+(enable-console-print!)
+
+(def app-state
+  (atom {:things []}))
+
+(defn virt-clj-app [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div nil
+               (dom/h1 nil "virt-clj is working!")))))
+
+(om/root app-state virt-clj-app (.getElementById js/document "content"))
