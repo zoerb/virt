a4ddb58531ff2b379cc87e7cdd7ac63b5c6d77c4
Ryan Zoerb <ryan.zoerb@gmail.com>
2014-03-16 22:30:48 -0500

    WIP: moving data up to the server

--- src/clj/virt/core.clj
+++ src/clj/virt/core.clj
@@ -8,11 +8,30 @@
             [ring.util.response :as resp]))
 
 
-(defn chat-init [ch]
-  (receive-all ch #(println "message: " %)))
+(def app-state
+  (atom {0x001 {:title "Cosm1"
+                :app :chat
+                :channels
+                {0x001 {:title "hi" :node-type :branch :children #{0xAA0 0xAA1 0xAA2}}
+                 0xAA0 {:title "hi1" :node-type :leaf :messages []}
+                 0xAA1 {:title "hi2" :node-type :leaf}
+                 0xAA2 {:title "hi3" :node-type :leaf}
+                 0x002 {:title "howdy" :node-type :branch}
+                 0x003 {:title "how's it goin" :node-type :branch}}}
+         0x002 {:title "Cosm2"
+                :app :chat
+                :channels
+                {0x101 {:title "one" :node-type :branch :children #{0xBA0}}
+                 0xBA0 {:title "another one!" :node-type :branch :children #{0xBA1}}
+                 0xBA1 {:title "another nother one!" :node-type :leaf :messages ["works?" "works."]}}}
+         0x003 {:title "Cosm3"
+                :app :chat
+                :channels {}}}))
+
 
 (defn chat-handler [ch id]
-  (let [chat (named-channel id chat-init)]
+  (let [chat (named-channel id nil)]
+    (enqueue ch "hi")
     (siphon chat ch)
     (siphon ch chat)))
 
